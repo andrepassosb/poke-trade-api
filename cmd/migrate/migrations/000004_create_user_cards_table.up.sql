d65a9a16864cfb821fdcc16a1ffdc0ffc83d0f89
André Passos <63965710+andrepassosb@users.noreply.github.com>
2025-04-04 09:36:08 -0300

    update tables

--- cmd/migrate/migrations/000004_create_user_cards_table.up.sql
+++ cmd/migrate/migrations/000004_create_user_cards_table.up.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS user_cards (
+CREATE TABLE user_cards_new (
     user_id INTEGER NOT NULL,
-    card_id INTEGER NOT NULL,
+    card_id TEXT NOT NULL,
     quantity INTEGER NOT NULL CHECK (quantity >= 0),
     PRIMARY KEY (user_id, card_id),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
